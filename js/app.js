715d91f40f49208057d6216defc690ad5ff2ad04
AndreaPotenza <andreapoten3@gmail.com>
2024-11-11 19:37:46 +0100

    completo + bonus

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 
 const emails = document.querySelector('.emails')
+const generate = document.querySelector('.generate-btn')
+console.log(generate)
 
-for (let i = 0; i < 10; i++) {
-    axios
-    .get('https://flynn.boolean.careers/exercises/api/random/mail') 
-    .then((res) => {
-        const mail = res.data.response
-        emails.innerHTML += `<li>${mail}</li>`
+function generateMails(mails, n = 10) {
 
-        
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+    for (let i = 0; i < 10; i++) {
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail') 
+        .then((res) => {
+            const mail = res.data.response
+            mails.innerHTML += `<li class="list-group-item">${mail}</li>`
+    
+            
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+    
 }
 
+generateMails(emails)
 
+generate.addEventListener('click', () => {
+    emails.innerHTML += ''
+    generateMails(emails)
+})
  
